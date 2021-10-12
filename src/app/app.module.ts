ea4c9f0486306d45a45a59d12c6c5ebc9da5eb85
danpuch7 <illia.demchishin@gmail.com>
2021-10-12 14:57:34 +0300

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,31 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { MainComponent } from './pages/main/main.component';
+import { DetailsComponent } from './pages/details/details.component';
+import {RouterModule} from "@angular/router";
+import {AppRoutingModule} from "./app-routing.module";
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import {HttpClientModule} from "@angular/common/http";
+import { HttpClientInMemoryWebApiModule } from 'angular-in-memory-web-api';
+import { InMemHeroService } from './services/in-memory-data.service';
+import { environment } from 'src/environments/environment';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainComponent,
+    DetailsComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule,
+    AppRoutingModule,
+    NgbModule,
+    HttpClientModule,
+    environment.production?[]: HttpClientInMemoryWebApiModule.forRoot(InMemHeroService),
   ],
-  providers: [],
+  providers: [InMemHeroService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
